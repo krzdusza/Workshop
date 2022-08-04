81fccb22a3653432e1b19b6486a12ca63d80301b
krzdusza <krzdusza@gmail.com>
2022-08-04 23:21:01 +0200

    view options added

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -3,5 +3,16 @@ package pl.coderslab;
 public class TaskManager {
     public static void main(String[] args) {
 
+        //  selectOption();
+        viewOptions();
     }
+
+    public static void viewOptions() {
+        System.out.println("\033[0;34m" + "Please select an option:" + "\033[0m");
+        String[] options = {"add", "remove", "list", "exit"};
+        for (String option : options) {
+            System.out.println(option);
+        }
+    }
+
 }
