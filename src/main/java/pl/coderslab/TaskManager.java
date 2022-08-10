e6136f4911ce0f337fb0943843e9a72f03156f85
krzdusza <krzdusza@gmail.com>
2022-08-10 22:50:20 +0200

    task add, list, exit added

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,19 +1,26 @@
 package pl.coderslab;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Scanner;
+
+import static pl.coderslab.colors.ConsoleColors.*;
 
 public class TaskManager {
     public static void main(String[] args) {
-
         tasks();
         viewOptions();
         selectOption();
     }
 
+    public static String[][] tasks = new String[0][];
+
     public static void viewOptions() {
-        System.out.println("\033[0;34m" + "Please select an option:" + "\033[0m");
+        System.out.println("\n" + BLUE + "Please select an option:" + RESET);
         String[] options = {"add", "remove", "list", "exit"};
         for (String option : options) {
             System.out.println(option);
@@ -61,7 +68,7 @@ public class TaskManager {
                 String line = scanner.nextLine();
                 String[] values = line.split(",");
                 for (int j = 0; j < values.length; j++) {
-                    tasks[i][j] = values[j];
+                    tasks[i][j] = values[j].trim();
                 }
                 i++;
             }
@@ -79,36 +86,91 @@ public class TaskManager {
         switch (selectOpt) {
             case "add":
                 System.out.println("add");
+                addTask();
+                viewOptions();
+                selectOption();
                 break;
             case "remove":
                 System.out.println("remove");
+                removeTask();
+                viewOptions();
+                selectOption();
                 break;
             case "list":
                 System.out.println("list");
                 listTask();
+                viewOptions();
+                selectOption();
                 break;
             case "exit":
                 System.out.println("exit");
                 exitTask();
                 break;
             default:
-                System.out.println("\033[1;91m" + "Incorrect input. Please select a correct option." + "\033[0m");
-                break;
+                System.out.println(RED_BOLD_BRIGHT + "Incorrect input. Please select a correct option." + RESET);
+                viewOptions();
+                selectOption();
         }
     }
 
     public static void listTask() {
         tasks();
         for (int i = 0; i < tasks().length; i++) {
-            System.out.print("\033[1;35m" + i + " : " + "\033[0m");
+            System.out.print(PURPLE_BOLD + i + " : " + RESET);
             for (int j = 0; j < tasks()[i].length; j++) {
-                System.out.print(tasks()[i][j]);
+                System.out.print(tasks()[i][j] + ", ");
             }
             System.out.println();
         }
     }
 
+    public static void addTask() {
+        Scanner scanner = new Scanner(System.in);
+        String newTask;
+        String newDate;
+        String newImportant;
+        do {
+            System.out.println("Please add task description");
+            newTask = scanner.nextLine();
+        } while (newTask.isEmpty());
+        do {
+            System.out.println("Please add task due date (format: YYYY-MM-DD)");
+            newDate = scanner.nextLine();
+        } while (!(newDate.startsWith("2") && newDate.startsWith("-", 4) && newDate.startsWith("-", 7)));
+        do {
+            System.out.println("Is your task is important: true/false");
+            newImportant = scanner.nextLine();
+        } while (!("true".equals(newImportant) || "false".equals(newImportant)));
+
+        tasks = ArrayUtils.add(tasks(), new String[]{newTask, newDate, newImportant});
+
+        try (PrintWriter printWriter = new PrintWriter("tasks.csv")) {
+            for (int i = 0; i < tasks.length; i++) {
+                for (int j = 0; j < tasks[i].length; j++) {
+                    printWriter.print(tasks[i][j] + ",");
+                }
+                printWriter.println();
+            }
+        } catch (FileNotFoundException ex) {
+            System.err.println("Błąd zapisu do pliku: " + ex.getLocalizedMessage());
+        }
+        System.out.println();
+        System.out.println(GREEN_BOLD + "Correct. Task was added." + RESET);
+        System.out.println();
+
+        tasks();
+    }
+
+    public static void removeTask() {
+        Scanner scanner = new Scanner(System.in);
+        String delTask;
+        System.out.println("Please select number to remove");
+        delTask = scanner.nextLine();
+        System.out.println("Value was successfully deleted");
+
+    }
+
     public static void exitTask() {
-        System.out.print("\033[0;31m" + " Bye, bye. " + "\033[0m");
+        System.out.print(RED + " Bye, bye. " + RESET);
     }
 }
